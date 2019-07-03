46ab6af6260b7b770db39fe7d6f7d79e800b2ce3
johncook <jonnymcook1@gmail.com>
2019-07-03 15:42:20 -0500

    second commit

--- first-project/App.js
+++ first-project/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, ImageBackground, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableHighlight, NavigatorIOS } from 'react-native';
 
 const Beaches = [
 
@@ -13,7 +13,15 @@ const Beaches = [
   },
   {
     url: "https://images.unsplash.com/photo-1501438400798-b40ff50396c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    caption: "Pina Colada anyone"
+    caption: "Pina Colada anyone?"
+  },
+  {
+    url: "https://images.unsplash.com/photo-1483683804023-6ccdb62f86ef?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60",
+    caption: "Island Peace"
+  },
+  {
+    url: "https://images.unsplash.com/photo-1414490929659-9a12b7e31907?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+    caption: "Surfs up"
   }
 
 ]
@@ -55,6 +63,7 @@ export default class App extends Component {
     const image= Beaches[this.state.index];
 
   return (
+
     <View style={styles.container}>
       <View style = {styles.empty}/>
         <TouchableHighlight onPress={this.newImage.bind(this)} style={styles.image}>
@@ -71,21 +80,27 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(52,165,218)',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+    // padding: 20
   },
   image: {
     flex: 2,
     width: 320,
     justifyContent: 'flex-end',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   imageCaption: {
       textAlign: 'center',
       backgroundColor: 'rgba(100, 100, 100, 0.5)',
       color: 'white',
       width: 320
+  },
+  header:{
+    flex: 3,
+    // paddingTop: 100,
+
   },
   empty: {
       flex: 1
